34da588302670cf51886bcf340a37203c88cd81e
Kinara <kinarasafina@gmail.com>
2026-01-07 14:15:23 +0100

    key gen attempt 1 but not working

--- swp/trial-1.py
+++ swp/trial-1.py
@@ -5,9 +5,10 @@ import numpy as np
 from numpy.polynomial import polynomial as p
 
 
-n = 4
+n = 4 # polynomial degree, so highest is x^3
 # q = 12289
-q = 31
+# q = 31
+q = 15 # 2^n - 1
 
 # modulus
 xN_1 = [1] + [0] * (n - 1) + [1]
@@ -32,7 +33,39 @@ A = np.floor(np.random.random(size=(n)) * q) % q
 A = np.floor(p.polydiv(A, xN_1)[1])  # [1] = taking the remainder value
 print(A)
 
+# ----ALICE-----
+print("----ALICE-----")
 eA = gen_poly(n, q)
 print(eA)
 sA = gen_poly(n, q)
 print(sA)
+
+# Alice now creates bA = (A x sA) + eA
+bA = p.polymul(A,sA)%q
+bA = np.floor(p.polydiv(bA,xN_1)[1])
+bA = p.polyadd(bA,eA)%q
+print(bA)
+
+#----BOB-----
+print("----BOB-----")
+sB = gen_poly(n,q)
+print(sB)
+eB = gen_poly(n,q)
+print(eB)
+
+bB = p.polymul(A,sB)%q
+bB = np.floor(p.polydiv(bB,xN_1)[1])
+bB = p.polyadd(bB,eB)%q
+print(bB)
+
+#----SHARED----
+print("----SHARED----")
+sharedAlice = np.floor(p.polymul(sA,bB)%q)
+sharedAlice = np.floor(p.polydiv(sharedAlice,xN_1)[1])%q
+print(sharedAlice)
+# 0, 0, 1, 0
+
+sharedBob = np.floor(p.polymul(sB,bA)%q)
+sharedBob = np.floor(p.polydiv(sharedBob,xN_1)[1])%q
+print(sharedBob)
+# 0, 0, 1, 1
