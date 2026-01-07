606bde6f7b9df815588f62bd31be877fc8e87370
Kinara <kinarasafina@gmail.com>
2026-01-07 15:26:17 +0100

    key gen success with error rounding and high q

--- swp/trial-1.py
+++ swp/trial-1.py
@@ -5,10 +5,10 @@ import numpy as np
 from numpy.polynomial import polynomial as p
 
 
-n = 4 # polynomial degree, so highest is x^3
+n = 10 # polynomial degree, so highest is x^3
 # q = 12289
 # q = 31
-q = 15 # 2^n - 1
+q = 1023 # 2^n - 1
 
 # modulus
 xN_1 = [1] + [0] * (n - 1) + [1]
@@ -29,8 +29,9 @@ def gen_poly(n, q):
     return poly
 
 
-A = np.floor(np.random.random(size=(n)) * q) % q
+A = np.floor(np.random.random(size=(n)) * q)
 A = np.floor(p.polydiv(A, xN_1)[1])  # [1] = taking the remainder value
+A = A % q
 print(A)
 
 # ----ALICE-----
@@ -41,9 +42,9 @@ sA = gen_poly(n, q)
 print(sA)
 
 # Alice now creates bA = (A x sA) + eA
-bA = p.polymul(A,sA)%q
-bA = np.floor(p.polydiv(bA,xN_1)[1])
-bA = p.polyadd(bA,eA)%q
+bA = p.polymul(A,sA)
+bA = p.polyadd(bA,eA)
+bA = np.floor(p.polydiv(bA,xN_1)[1])%q
 print(bA)
 
 #----BOB-----
@@ -53,19 +54,88 @@ print(sB)
 eB = gen_poly(n,q)
 print(eB)
 
-bB = p.polymul(A,sB)%q
-bB = np.floor(p.polydiv(bB,xN_1)[1])
-bB = p.polyadd(bB,eB)%q
+bB = p.polymul(A,sB)
+bB = p.polyadd(bB,eB)
+bB = np.floor(p.polydiv(bB,xN_1)[1])%q
 print(bB)
 
 #----SHARED----
 print("----SHARED----")
-sharedAlice = np.floor(p.polymul(sA,bB)%q)
+sharedAlice = np.floor(p.polymul(sA,bB))
 sharedAlice = np.floor(p.polydiv(sharedAlice,xN_1)[1])%q
 print(sharedAlice)
-# 0, 0, 1, 0
 
-sharedBob = np.floor(p.polymul(sB,bA)%q)
+sharedBob = np.floor(p.polymul(sB,bA))
 sharedBob = np.floor(p.polydiv(sharedBob,xN_1)[1])%q
 print(sharedBob)
-# 0, 0, 1, 1
+
+
+#----VERIFICATION----
+def construct_u_helper(poly, q):
+    bits = []
+    q4 = q / 4
+    q2 = q / 2
+    q34 = 3 * q / 4
+
+    for c in poly:
+        if c < q4:
+            bits.append(0)
+        elif c < q2:
+            bits.append(1)
+        elif c < q34:
+            bits.append(0)
+        else:
+            bits.append(1)
+
+    return bits
+
+u = construct_u_helper(sharedBob, q)
+print(u)
+
+# --Bob
+i = 0
+while (i < len(u)):
+	#Region 0 (0 --- q/4 and q/2 --- 3q/4)
+	if (u[i] == 0):
+		if (sharedBob[i] >= q*0.125 and sharedBob[i] < q*0.625):
+			sharedBob[i] = 1
+		else:
+			sharedBob[i] = 0
+
+
+	#Region 1 (q/4 --- q/2 and 3q/4 --- q)
+	elif (u[i] == 1):
+		if (sharedBob[i] >= q*0.875 and sharedBob[i] < q*0.375):
+			sharedBob[i] = 0
+		else:
+			sharedBob[i] = 1
+
+	else:
+		print("error! (2)")
+
+	i += 1
+
+#--Alice
+i = 0
+while (i < len(u)):
+	#Region 0 (0 --- q/4 and q/2 --- 3q/4)
+	if (u[i] == 0):
+		if (sharedAlice[i] >= q*0.125 and sharedAlice[i] < q*0.625):
+			sharedAlice[i] = 1
+		else:
+			sharedAlice[i] = 0
+
+
+	#Region 1 (q/4 --- q/2 and 3q/4 --- q)
+	elif (u[i] == 1):
+		if (sharedAlice[i] >= q*0.875 and sharedAlice[i] < q*0.375):
+			sharedAlice[i] = 0
+		else:
+			sharedAlice[i] = 1
+
+	else:
+		print("error! (3)")
+	i += 1
+	
+print(sharedAlice)
+print(sharedBob)
